3c86be1cda63a887e36ead30db91f064bdeb0aa7
Tien Do <forever.love.wisdom@gmail.com>
2020-06-18 16:52:13 +0700

    Getting key or values in hash table takes O(n)

--- hash_table.rb
+++ hash_table.rb
@@ -10,8 +10,31 @@ class HashTable
 
     unless data[address]
       data[address] = []
-      data[address].push([key, value])
     end
+    data[address].push([key, value])
+  end
+
+  def get(key)
+    address = get_hash(key)
+    data[address]&.each do |pairs|
+      return pairs.last if pairs.first == key
+    end
+  end
+
+  def keys
+    # Time complexity could be O(n^2) theoretically. However, it is unlikely to happen
+    # Because in order for that to happen, they hash collision happens for all of the keys.
+    # Something must be wrong with the hash function then.
+    hash_keys = []
+    data.each do |dt|
+      next unless dt
+
+      dt.each do |pair|
+        hash_keys << pair[0]
+      end
+    end
+
+    hash_keys
   end
 
   private
@@ -31,4 +54,8 @@ hash_table = HashTable.new(2)
 puts "The computed key is: #{hash_table.instance_eval { get_hash(key) }}"
 hash_table.set(key, 5)
 hash_table.set('grape', 15)
+hash_table.set('grapes', 888)
+hash_table.set('google', 99)
 print hash_table.data
+print "\n"
+print hash_table.keys
